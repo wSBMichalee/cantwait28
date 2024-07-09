ad0952c173087c9db521be26f8ae01ec44926975
MichalC <micukr23@gmail.com>
2024-07-09 15:57:10 +0200

    Implement user_profile page

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:cantwait28/features/add/page/add_page.dart';
+import 'package:cantwait28/features/auth/user_profile.dart';
 import 'package:cantwait28/features/details/pages/details_page.dart';
 import 'package:cantwait28/features/home/cubit/home_cubit.dart';
 import 'package:cantwait28/models/item_model.dart';
@@ -16,6 +17,17 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Can\'t Wait 🤩'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => UserProfile(),
+                  ),
+                );
+              },
+              icon: Icon(Icons.person))
+        ],
       ),
       body: const _HomePageBody(),
       floatingActionButton: FloatingActionButton(
